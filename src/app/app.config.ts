a29d36f9308d40abc3d8040350b16b8a87212071
Mahmoud <mahmoudtarekmahmoud44@gmail.com>
2024-05-05 17:16:39 +0300

    build: First commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,55 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  provideHttpClient,
+  withInterceptors,
+  HttpBackend,
+} from '@angular/common/http';
+import {
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withViewTransitions,
+  withRouterConfig,
+  provideRouter,
+} from '@angular/router';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 
 import { routes } from './app.routes';
 
+const MultiFileTranslateLoader = (
+  http: HttpBackend
+): MultiTranslateHttpLoader => new MultiTranslateHttpLoader(http, []);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideAnimationsAsync(),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+        onSameUrlNavigation: 'reload',
+      }),
+      withComponentInputBinding(),
+      withViewTransitions()
+    ),
+    provideHttpClient(withInterceptors([])),
+    importProvidersFrom(
+      MatDatepickerModule,
+      MatNativeDateModule,
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: MultiFileTranslateLoader,
+          deps: [HttpBackend],
+        },
+      })
+    ),
+  ],
 };
