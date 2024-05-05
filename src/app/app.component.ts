cf122852448bef05f7508c6103205f094a53e97f
Mahmoud <mahmoudtarekmahmoud44@gmail.com>
2024-05-05 17:42:08 +0300

    docs: add constructor phase into lifecycle explanation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,41 +12,50 @@ import { TranslateModule } from '@ngx-translate/core';
 export class AppComponent {
   title = 'Majales-demo';
 
-  constructor() {}
+  constructor() {
+    console.log('Constructor phase');
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
+    console.log('ngOnChanges phase');
   }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+    console.log('ngOnInit phase');
   }
 
   ngDoCheck(): void {
     //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
     //Add 'implements DoCheck' to the class.
+    console.log('ngDoCheck phase');
   }
 
   ngAfterContentInit(): void {
     //Called after ngOnInit when the component's or directive's content has been initialized.
     //Add 'implements AfterContentInit' to the class.
+    console.log('ngAfterContentInit phase');
   }
 
   ngAfterContentChecked(): void {
     //Called after every check of the component's or directive's content.
     //Add 'implements AfterContentChecked' to the class.
+    console.log('ngAfterContentChecked phase');
   }
 
   ngAfterViewInit(): void {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
+    console.log('ngAfterViewInit phase');
   }
 
   ngAfterViewChecked(): void {
     //Called after every check of the component's view. Applies to components only.
     //Add 'implements AfterViewChecked' to the class.
+    console.log('ngAfterViewChecked phase');
   }
 
   ngOnDestroy(): void {
